f6788955c387648a670c586451330578392e1d68
Atharv Pathak <78468172+Atharv-Pathak-14@users.noreply.github.com>
2022-10-03 22:55:22 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,15 +1,5 @@
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
 jabba install zulu@1.13.0-2
 
-
-if [ ! -f "Lavalink.jar" ]; then
-    curl --output  Lavalink.jar "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev"
+wget Lavalink.jar https://github.com/freyacodes/Lavalink/releases/download/3.5.1/Lavalink.jar
     java -jar Lavalink.jar
-else
-  if [ "$AUTO_UPDATE" = "true" ]; then
-    curl --output  Lavalink.jar "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev"
-    java -jar Lavalink.jar
-  else
-    java -jar Lavalink.jar
-  fi
-fi
