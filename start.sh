34d50d799e457bc17197f22060929c33e05e1ef8
Atharv Pathak <78468172+Atharv-Pathak-14@users.noreply.github.com>
2023-05-27 10:41:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 curl -sL https://github.com/shyiko/jabba/raw/master/install.sh | bash -s -- --skip-rc && . ~/.jabba/jabba.sh
 jabba install zulu@1.13.0-2
 
-wget Lavalink.jar https://github.com/freyacodes/Lavalink/releases/download/3.5.1/Lavalink.jar
+wget Lavalink.jar https://github.com/lavalink-devs/Lavalink/releases/download/3.7.5/Lavalink.jar
     java -jar Lavalink.jar
